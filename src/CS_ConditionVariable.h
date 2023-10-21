f476d298ed10675daf776ae2fc557bc59667db73
unknownopponent <unknown.opponent.contact@gmail.com>
2023-10-21 22:12:43 +0200

    changed condition variable, added condition variable mutex, refactor tests

--- src/CS_ConditionVariable.h
+++ src/CS_ConditionVariable.h
@@ -1,33 +1,82 @@
 #pragma once
 
 #include <string.h> // memset
+#include <assert.h>
 
 #include "common.h"
 #include "CS_Mutex.h"
 
+#if defined(C11_THREADS) || defined(POSIX_THREADS)
+typedef CS_Mutex CS_ConditionVariableMutex;
+#elif defined(WIN32_THREADS)
+typedef SRWLOCK CS_ConditionVariableMutex;
+#endif
+
+char cscvm_create(CS_ConditionVariableMutex* mutex);
+char cscvm_lock(CS_ConditionVariableMutex* mutex);
+char cscvm_unlock(CS_ConditionVariableMutex* mutex);
+char cscvm_destroy(CS_ConditionVariableMutex* mutex);
+
+inline char cscvm_create(CS_ConditionVariableMutex* mutex)
+{
+#if defined(C11_THREADS) || defined(POSIX_THREADS)
+	return csm_create(mutex);
+#elif defined(WIN32_THREADS)
+	InitializeSRWLock(mutex);
+	return 0;
+#endif
+}
+
+inline char cscvm_lock(CS_ConditionVariableMutex* mutex)
+{
+#if defined(C11_THREADS) || defined(POSIX_THREADS)
+	return csm_lock(mutex);
+#elif defined(WIN32_THREADS)
+	AcquireSRWLockExclusive(mutex);
+	return 0;
+#endif
+}
+
+inline char cscvm_unlock(CS_ConditionVariableMutex* mutex)
+{
+#if defined(C11_THREADS) || defined(POSIX_THREADS)
+	return csm_unlock(mutex);
+#elif defined(WIN32_THREADS)
+	ReleaseSRWLockExclusive(mutex);
+	return 0;
+#endif
+}
+
+inline char cscvm_destroy(CS_ConditionVariableMutex* mutex)
+{
+#if defined(C11_THREADS) || defined(POSIX_THREADS)
+	return csm_destroy(mutex);
+#elif defined(WIN32_THREADS)
+	memset(mutex, 0, sizeof(CS_ConditionVariableMutex));
+	return 0;
+#endif
+}
+
 typedef struct CS_ConditionVariable {
 	
+	CS_ConditionVariableMutex* mutex;
+	
 #ifdef C11_THREADS
-	CS_Mutex mutex;
 	cnd_t cv_handle;
 #endif
 #ifdef WIN32_THREADS
-	SRWLOCK mutex_handle;
 	CONDITION_VARIABLE cv_handle;
 #endif
 #ifdef POSIX_THREADS
-	CS_Mutex mutex;
 	pthread_cond_t cv_handle;
 #endif
 
 } CS_ConditionVariable;
 
+
 char cscv_create(CS_ConditionVariable* cv);
 
-char cscv_lock_to_wait(CS_ConditionVariable* cv);
 char cscv_wait(CS_ConditionVariable* cv);
-char cscv_unlock(CS_ConditionVariable* cv);
-
 char cscv_signal(CS_ConditionVariable* cv);
 
 char cscv_destroy(CS_ConditionVariable* cv);
@@ -35,126 +84,62 @@ char cscv_destroy(CS_ConditionVariable* cv);
 inline char cscv_create(CS_ConditionVariable* cv)
 {
 #ifdef C11_THREADS
-	if (csm_create(&cv->mutex))
-	{
-		return 1;
-	}
-	
 	return cnd_init(&cv->cv_handle) != thrd_success;
 #endif
 #ifdef WIN32_THREADS
-	InitializeSRWLock(&cv->mutex_handle);
 	InitializeConditionVariable(&cv->cv_handle);
 	return 0;
 #endif
 #ifdef POSIX_THREADS
-	if (csm_create(&cv->mutex))
-	{
-		return 1;
-	}
-	
 	return pthread_cond_init(&cv->cv_handle, 0);
 #endif
 }
 
-inline char cscv_lock_to_wait(CS_ConditionVariable* cv)
-{
-#if defined(C11_THREADS) || defined(POSIX_THREADS)
-	return csm_lock(&cv->mutex);
-#elif defined(WIN32_THREADS)
-	AcquireSRWLockExclusive(&cv->mutex_handle);
-	return 0;
-#endif
-	
-}
-
 inline char cscv_wait(CS_ConditionVariable* cv)
 {
+	assert(cv->mutex);
 #ifdef C11_THREADS
-	return cnd_wait(&cv->cv_handle, &cv->mutex.mutex_handle) != thrd_success;
+	return cnd_wait(&cv->cv_handle, &cv->mutex->mutex_handle) != thrd_success;
 #endif
 #ifdef WIN32_THREADS
-	return !SleepConditionVariableSRW(&cv->cv_handle, &cv->mutex_handle, INFINITE, 0);
+	return !SleepConditionVariableSRW(&cv->cv_handle, cv->mutex, INFINITE, 0);
 #endif
 #ifdef POSIX_THREADS
-	return pthread_cond_wait(&cv->cv_handle, &cv->mutex.mutex_handle);
-#endif
-}
-
-inline char cscv_unlock(CS_ConditionVariable* cv)
-{
-#if defined(C11_THREADS) || defined(POSIX_THREADS)
-	return csm_unlock(&cv->mutex);
-#elif defined(WIN32_THREADS)
-	ReleaseSRWLockExclusive(&cv->mutex_handle);
-	return 0;
+	return pthread_cond_wait(&cv->cv_handle, &cv->mutex->mutex_handle);
 #endif
 }
 
 inline char cscv_signal(CS_ConditionVariable* cv)
 {
 #ifdef C11_THREADS
-	if (csm_lock(&cv->mutex))
-	{
-		return 1;
-	}
-	
-	if (cnd_signal(&cv->cv_handle) != thrd_success)
-	{
-		return 1;
-	}
-	
-	return csm_unlock(&cv->mutex);
+	return cnd_signal(&cv->cv_handle) != thrd_success;
 #endif
 #ifdef WIN32_THREADS
 	WakeConditionVariable(&cv->cv_handle);
 	return 0;
 #endif
 #ifdef POSIX_THREADS
-	if (csm_lock(&cv->mutex))
-	{
-		return 1;
-	}
-
-	if (pthread_cond_signal(&cv->cv_handle))
-	{
-		return 1;
-	}
-	
-	return csm_unlock(&cv->mutex);
+	return pthread_cond_signal(&cv->cv_handle);
 #endif
 }
 
 inline char cscv_destroy(CS_ConditionVariable* cv)
 {
 #ifdef C11_THREADS
-	if (csm_destroy(&cv->mutex))
-	{
-		return 1;
-	}
-	
 	cnd_destroy(&cv->cv_handle);
 	memset(&cv->cv_handle, 0, sizeof(cnd_t));
-	
 	return 0;
 #endif
 #ifdef WIN32_THREADS
 	memset(&cv->cv_handle, 0, sizeof(CONDITION_VARIABLE));
-	memset(&cv->mutex_handle, 0, sizeof(SRWLOCK));
 	return 0;
 #endif
 #ifdef POSIX_THREADS
-	if (csm_destroy(&cv->mutex))
-	{
-		return 1;
-	}
-
 	if (pthread_cond_destroy(&cv->cv_handle))
 	{
 		return 1;
 	}
 	memset(&cv->cv_handle, 0, sizeof(pthread_cond_t));
-	
 	return 0;
 #endif
 }
--- test/test.c
+++ test/test.c
@@ -13,57 +13,255 @@ void print_error(char* str)
 	fwrite(str, sizeof(char), strlen(str), stderr);
 }
 
-typedef struct Dummy
+void thread_write_int(int* integer)
 {
-	CS_Mutex mutex;
-	CS_ConditionVariable cv;
-	int test1;
-	int test2;
-
-} Dummy;
-
-void dummy_thread_write(Dummy* dummy)
-{
-	if (!csm_try_lock(&dummy->mutex))
-	{
-		print_error("try lock succed while fail was expected\n");
-		exit(1);
-	}
-	
-	if (cs_sleep_seconds(0.5))
-	{
-		print_error("failled to sleep\n");
-		exit(1);
-	}
-
-	if (cscv_signal(&dummy->cv))
-	{
-		print_error("condition variable signal failled\n");
-		exit(1);
-	}
-
-	dummy->test1 = 1;
-
-	if (csm_lock(&dummy->mutex))
-	{
-		dummy->test2 = 3;
-		cst_exit(3);
-	}
-
-	dummy->test2 = 4;
-
-	if (csm_unlock(&dummy->mutex))
-	{
-		print_error("failled to unlock in thread mutex\n");
-		exit(1);
-	}
-
-	puts("thread exit normally");
-
-	cst_exit(4);
+	*integer = 2;
+	cst_exit(3);
 }
 
-int main(void)
+char test_thread()
+{
+	int test = -1;
+	
+	CS_Thread thread = { 0 };
+	thread.function = thread_write_int;
+	thread.args = &test;
+	
+	if (cst_create(&thread))
+	{
+		print_error("failled to create thread\n");
+		return 1;
+	}
+	
+	int ret = -1;
+	
+	if (cst_join(&thread, &ret))
+	{
+		print_error("failled to join thread\n");
+		return 1;
+	}
+	
+	if (test != 2)
+	{
+		fprintf(stderr, "thread did not write expected value %d\n", test);
+		return 1;
+	}
+	
+	if (ret != 3)
+	{
+		fprintf(stderr, "thread did not return expected value %d\n", ret);
+		return 1;
+	}
+	
+	return 0;
+}
+
+typedef struct ThreadMutexContext
+{
+	CS_Mutex mutex;
+	int integer;
+} ThreadMutexContext;
+
+void thread_mutex_write(ThreadMutexContext* ctx)
+{
+	if (csm_lock(&ctx->mutex))
+	{
+		print_error("thread failled to lock mutex\n");
+		cst_exit(1);
+	}
+
+	ctx->integer = 3;
+
+	if (csm_unlock(&ctx->mutex))
+	{
+		print_error("thread failled to unlock mutex\n");
+		cst_exit(1);
+	}
+	
+	cst_exit(0);
+}
+
+char test_mutex()
+{
+	ThreadMutexContext ctx = {0};
+	ctx.integer = -1;
+	
+	if (csm_create(&ctx.mutex))
+	{
+		print_error("failled to create mutex\n");
+		return 1;
+	}
+	
+	if (csm_lock(&ctx.mutex))
+	{
+		print_error("failled to lock mutex\n");
+		return 1;
+	}
+
+	CS_Thread thread = { 0 };
+	thread.function = thread_mutex_write;
+	thread.args = &ctx;
+	
+	if (cst_create(&thread))
+	{
+		print_error("failled to create thread\n");
+		return 1;
+	}
+	
+	if (cs_sleep_seconds(0.2))
+	{
+		print_error("failled to sleep\n");
+		return 1;
+	}
+	
+	ctx.integer = 3;
+	
+	if (csm_unlock(&ctx.mutex))
+	{
+		print_error("failled to unlock mutex\n");
+		return 1;
+	}
+	
+	int ret = -1;
+	
+	if (cst_join(&thread, &ret))
+	{
+		print_error("failled to join thread\n");
+		return 1;
+	}
+	
+	if (ret != 0)
+	{
+		fprintf(stderr, "thread did not return expected value %d\n", ret);
+		return 1;
+	}
+	
+	if (ctx.integer != 3)
+	{
+		fprintf(stderr, "thread did not write expected value %d\n", ctx.integer);
+		return 1;
+	}
+	
+	if (csm_destroy(&ctx.mutex))
+	{
+		print_error("failled to destroy mutex\n");
+		return 1;
+	}
+	
+	return 0;
+}
+
+typedef struct TreadSignalContext
+{
+	CS_ConditionVariableMutex mutex;
+	CS_ConditionVariable cv;
+} TreadSignalContext;
+
+void thread_signal(TreadSignalContext* ctx)
+{
+	if (cs_sleep_seconds(0.2))
+	{
+		print_error("failled to sleep\n");
+		cst_exit(1);
+	}
+	
+	if (cscvm_lock(&ctx->mutex))
+	{
+		print_error("thread failled to lock mutex\n");
+		cst_exit(1);
+	}
+
+	if (cscv_signal(&ctx->cv))
+	{
+		print_error("thread failled to signal condition variable\n");
+		cst_exit(1);
+	}
+
+	if (cscvm_unlock(&ctx->mutex))
+	{
+		print_error("thread failled to unlock mutex\n");
+		cst_exit(1);
+	}
+
+	cst_exit(0);
+}
+
+char test_condition_variable()
+{
+	TreadSignalContext ctx = {0};
+	
+	if (cscvm_create(&ctx.mutex))
+	{
+		print_error("failled to create mutex\n");
+		return 1;
+	}
+	
+	if (cscv_create(&ctx.cv))
+	{
+		print_error("failled to create condition variable\n");
+		return 1;
+	}
+	
+	ctx.cv.mutex = &ctx.mutex;
+	
+	if (cscvm_lock(&ctx.mutex))
+	{
+		print_error("failled to lock mutex\n");
+		return 1;
+	}
+	
+	CS_Thread thread = { 0 };
+	thread.function = thread_signal;
+	thread.args = &ctx;
+	
+	if (cst_create(&thread))
+	{
+		print_error("failled to create thread\n");
+		return 1;
+	}
+	
+	if (cscv_wait(&ctx.cv))
+	{
+		print_error("failled to wait condition variable\n");
+		return 1;
+	}
+	
+	if (cscvm_unlock(&ctx.mutex))
+	{
+		print_error("failled to unlock mutex\n");
+		return 1;
+	}
+	
+	int ret = -1;
+	
+	if (cst_join(&thread, &ret))
+	{
+		print_error("failled to join thread\n");
+		return 1;
+	}
+	
+	if (ret != 0)
+	{
+		fprintf(stderr, "thread did not return expected value %d\n", ret);
+		return 1;
+	}
+	
+	if (cscv_destroy(&ctx.cv))
+	{
+		print_error("failled to destroy condition variable\n");
+		return 1;
+	}
+	
+	if (cscvm_destroy(&ctx.mutex))
+	{
+		print_error("failled to destroy mutex\n");
+		return 1;
+	}
+	
+	return 0;
+}
+
+int main()
 {
 #ifdef C11_THREADS
 	puts("using C11 threads");
@@ -76,98 +274,24 @@ int main(void)
 #endif
 
 	printf("%d cpu threads\n", cs_cpu_thread_count());
-
-	Dummy test = { 0 };
-
-	if (csm_create(&test.mutex))
+	
+	if (test_thread())
 	{
-		print_error("failled to create mutex\n");
+		print_error("thread test failled\n");
 		return 1;
 	}
-
-	if (csm_lock(&test.mutex))
+	if (test_mutex())
 	{
-		print_error("failled to lock mutex\n");
+		print_error("mutex test failled\n");
+		return 1;
+	}
+	if (test_condition_variable())
+	{
+		print_error("condition variable test failled\n");
 		return 1;
 	}
 	
-	if (cscv_create(&test.cv))
-	{
-		print_error("failled to create condition variable\n");
-		return 1;
-	}
-	if (cscv_lock_to_wait(&test.cv))
-	{
-		print_error("failled to lock condition varaible mutex\n");
-		return 1;
-	}
-
-	CS_Thread thread = { 0 };
-	thread.function = dummy_thread_write;
-	thread.args = &test;
-
-	if (cst_create(&thread))
-	{
-		print_error("failled to create thread\n");
-		return 1;
-	}
-
-	if (cscv_wait(&test.cv))
-	{
-		print_error("failled to wait condition variable\n");
-		return 1;
-	}
+	puts("tests passed succesfully\n");
 	
-	if (cscv_unlock(&test.cv))
-	{
-		print_error("failled to unlock condition variable mutex\n");
-		return 1;
-	}
-	
-	if (cs_sleep_seconds(0.5))
-	{
-		print_error("failled to sleep\n");
-		return 1;
-	}
-
-	if (csm_unlock(&test.mutex))
-	{
-		print_error("failled to unlock mutex\n");
-		return 1;
-	}
-	
-	int ret = 0;
-
-	if (cst_join(&thread, &ret))
-	{
-		print_error("failled to join thread\n");
-		return 1;
-	}
-
-	if (test.test1 != 1)
-	{
-		fprintf(stderr, "wrong value in memory %d, 1 expected\n", test.test1);
-		return 1;
-	}
-	if (test.test2 != 4)
-	{
-		print_error("thread failled to lock mutex\n");
-		return 1;
-	}
-
-	if (ret != 4)
-	{
-		fprintf(stderr, "wrong return code %d, 4 expected\n", ret);
-		return 1;
-	}
-
-	if (csm_destroy(&test.mutex))
-	{
-		print_error("failled to destroy mutex");
-		return 1;
-	}
-
-	puts("thread tests passed");
-
 	return 0;
 }
